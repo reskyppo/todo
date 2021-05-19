191f19d72f920e13b8be1f4f920a176f41fb58d0
Reskyppo <reskybeen@gmail.com>
2021-05-19 09:28:01 +0700

    add data dummy Todo

---
+++ src/utils/data.js
@@ -0,0 +1,22 @@
+const Todo = [
+  {
+    title: "Homepage",
+    desc: "slicing homepage desktop",
+    label: "Frontend",
+    date: "Jun 22",
+  },
+  {
+    title: "Homepage",
+    desc: "desain homepage mobile",
+    label: "UI Design",
+    date: "Jun 21",
+  },
+  {
+    title: "Homepage",
+    desc: "API Card Data",
+    label: "Backend",
+    date: "Jun 23",
+  },
+];
+
+export { Todo };
