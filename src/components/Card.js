245cafd834777367abd277017fda61a21d76ffd2
Reskyppo <reskybeen@gmail.com>
2021-05-19 10:13:09 +0700

    add props data into card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 import ICPlus from "../assets/ic-plus.svg";
 import ICCLock from "../assets/ic-clock.svg";
 
-const Card = ({ title, addBtn }) => {
+const Card = ({ title, addBtn, datas }) => {
   return (
     <div className="w-1/5">
       <div className="bg-grey-100 rounded-md p-4">
@@ -18,22 +18,19 @@ const Card = ({ title, addBtn }) => {
             </div>
           )}
         </div>
-        <div className="bg-white mt-4 p-4 rounded-md">
-          <h2 className="text-md font-medium">judul</h2>
-          <p className="mt-2">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste, ab
-            soluta enim neque veritatis dolore numquam ea culpa nesciunt, vero
-            quisquam illum voluptate dolor rerum iure officiis harum
-            voluptatibus ipsa?
-          </p>
-          <div className="mt-2 w-1/4 rounded-sm py-1 font-medium text-center text-blue-2 ring-1 ring-blue-2 ring-opacity-50 bg-blue-1 ">
-            Frontend
+        {datas?.map((data, idx) => (
+          <div className="bg-white mt-4 p-4 rounded-md" key={idx}>
+            <h2 className="text-md font-medium">{data.title}</h2>
+            <p className="mt-2">{data.desc}</p>
+            <div className="mt-2 w-1/4 rounded-sm py-1 font-medium text-sm text-center text-blue-2 ring-1 ring-blue-2 ring-opacity-50 bg-blue-1 ">
+              {data.label}
+            </div>
+            <div className="flex items-center mt-2">
+              <img src={ICCLock} alt="Clock's Icon" className="w-4" />
+              <p className="text-md font-medium ml-2">{data.date}</p>
+            </div>
           </div>
-          <div className="flex items-center mt-2">
-            <img src={ICCLock} alt="Clock's Icon" className="w-4" />
-            <p className="text-md font-medium ml-2">Jun 21</p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Card from "./Card";
-
+import { Todo } from "../utils/data";
 const Main = () => {
   return (
     <div className="mt-8 mx-16">
       <h1 className="text-2xl font-medium">Task Manager</h1>
       <div className="flex flex-row justify-between mt-8">
-        <Card title="To do" addBtn />
+        <Card title="To do" addBtn datas={Todo} />
         <Card title="In Progress" />
         <Card title="In Review" />
         <Card title="Done" />
